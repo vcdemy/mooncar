72ba4b4b442318c8b7213f3e320825efb38701c2
Victor Gau <victorgau@gmail.com>
2023-05-31 16:07:51 +0800

    Upload test_color.py

--- py/colors/test_sensor.py
+++ py/colors/test_sensor.py
@@ -1,14 +1,22 @@
 # Imports go at the top
 from microbit import *
 import colors
+import neopixel
 
 sensors = colors.init_i2c()
 sensor = sensors[0]
 colors.setup(sensor)
+np = neopixel.NeoPixel(pin12, 8)
+
+np.fill((255, 0, 0))
+np.show()
 
 while True:
     colors.light_on()
     sleep(1000)
-    print(colors.read_color(sensor))
+    color = colors.read_color(sensor)
+    print(color)
+    np.fill(color)
+    np.show()
     colors.light_off()
     sleep(1000)
